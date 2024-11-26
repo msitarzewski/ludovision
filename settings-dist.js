7c41c8fc90731deac3fc85ee19a121b17194bc39
Michael Sitarzewski <msitarzewski@users.noreply.github.com>
2024-11-26 02:41:29 -0600

    Magnification, authentication, and much more

--- settings-dist.js
+++ settings-dist.js
@@ -1,6 +1,24 @@
-settings = {
-    token: null,
-    imageSize: 128,
+// Bluesky credentials from Account Settings
+// see https://bsky.app/settings/app-passwords
+const bsky_identifier = null;
+const bsky_appPassword = null;
+
+// Default feed image size
+const imageSize = 128;
+
+// Launch warning display on load/refresh
+const launchWarning = true;
+
+// Magnifier in Gallery view
+const magnifier = true;
+
+// Settings object
+const settings = {
+    imageSize: imageSize,
+    launchWarning: launchWarning,
+    magnifier: magnifier,
+    bsky_identifier: bsky_identifier,
+    bsky_appPassword: bsky_appPassword,
 }
     
 window.settings = settings;
